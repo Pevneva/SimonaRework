fd4fe38dff8f263d552e2923393906750e3ad0f6
Lyudmila5733 <millo4ka@list.ru>
2023-02-05 12:35:32 +0100

    Added camera following.

---
+++ Assets/CodeBase/CameraLogic/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.CameraLogic
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform _following;
+
+        private void LateUpdate()
+        {
+            transform.position = new Vector3(_following.position.x, _following.position.y, -10);
+        }
+    }
+}
