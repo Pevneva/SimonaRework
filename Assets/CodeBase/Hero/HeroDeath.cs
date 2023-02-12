4c8311208392f48afe79e424f9741fd7c9443096
Lyudmila5733 <millo4ka@list.ru>
2023-02-12 09:46:21 +0100

    Added HeroDeath logic.

---
+++ Assets/CodeBase/Hero/HeroDeath.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    public class HeroDeath : MonoBehaviour
+    {
+        [SerializeField] private HeroAnimator _animator;
+        [SerializeField] private HeroMover _mover;
+        [SerializeField] private HeroHealth _health;
+
+        private bool _isDead;
+
+        private void Start() => _health.HealthChanged += CheckDeath;
+
+        private void OnDestroy() => _health.HealthChanged -= CheckDeath;
+
+        private void CheckDeath()
+        {
+            if (_isDead == false && _health.Current <= 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            _isDead = true;
+            _mover.enabled = false;
+            _animator.PlayDeath();
+        }
+    }
+}
